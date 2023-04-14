94c8dd024cfa183a2a455f3f845c7e1da21cf7cc
Manu Meixell <misternu@gmail.com>
2023-04-14 09:59:31 -0500

    server 0

--- src/server_0.rb
+++ src/server_0.rb
@@ -1,1 +1,11 @@
-puts "Hello World"
+require 'socket'            
+
+server = TCPServer.new 4242
+loop do
+  Thread.start(server.accept) do |client|
+    p client
+    p data = client.gets
+    client.puts(data)
+    client.close
+  end
+end
